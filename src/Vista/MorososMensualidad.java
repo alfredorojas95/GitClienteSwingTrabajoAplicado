6ac3d7e2553f115e55e117ee4e65da604c1674d8
wilfri95 <wilfri_95@hotmail.com>
2015-08-09 01:44:00 -0400

    reportes sueldo y mensualidad

--- src/Vista/MorososMensualidad.java
+++ src/Vista/MorososMensualidad.java
@@ -11,16 +11,23 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 import javax.swing.LayoutStyle.ComponentPlacement;
+
 import java.awt.Font;
+
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
+import com.google.gson.Gson;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.rmi.RemoteException;
 
+import serviciomensualidad.ServicioMensualidadProxy;
 public class MorososMensualidad extends JFrame {
 
 	private JPanel contentPane;
@@ -58,14 +65,49 @@ public class MorososMensualidad extends JFrame {
 		lblSecretaria.setFont(new Font("Comic Sans MS", Font.ITALIC, 22));
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"}));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Mes", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"}));
 		
 		JLabel lblMorososMensualidad = new JLabel("Morosos Mensualidad");
 		lblMorososMensualidad.setFont(new Font("Comic Sans MS", Font.ITALIC, 15));
 		
+		JLabel labelResultado = new JLabel("");
+		
 		JButton btnVer = new JButton("Ver");
 		btnVer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				visible();
+				int dato = comboBox.getSelectedIndex();
+				if(dato>0){
+					ServicioMensualidadProxy morososMens = new ServicioMensualidadProxy();
+					
+					String[][] matriz;
+					Gson gson = new Gson();
+					String jsonMorosos;
+					try {
+						jsonMorosos = morososMens.obtenerListMorososMensualidad(dato);
+						if(jsonMorosos!=null){
+							matriz = gson.fromJson(jsonMorosos, String[][].class);
+							Object [] fila = new Object[4]; fila[0] = "unal";
+							
+							for (int i = 0; i < matriz.length; i++) {
+							fila[0] = matriz[i][0];
+							fila[1] = matriz[i][1];
+							fila[2] = matriz[i][2];
+							fila[3] = matriz[i][3];
+
+						
+							((DefaultTableModel) table.getModel()).addRow(fila);
+						}
+							
+						}else {
+							labelResultado.setText("La tabla se encuentra vacía de momento");
+					
+					}
+					} catch (RemoteException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
 			}
 		});
 		
@@ -78,9 +120,11 @@ public class MorososMensualidad extends JFrame {
 				setVisible(false);
 			}
 		});
+		
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap(172, Short.MAX_VALUE)
 					.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
@@ -96,13 +140,17 @@ public class MorososMensualidad extends JFrame {
 					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 446, GroupLayout.PREFERRED_SIZE)
 					.addGap(50))
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(219)
-					.addComponent(btnVolverAlMen, GroupLayout.PREFERRED_SIZE, 125, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(220, Short.MAX_VALUE))
-				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap(233, Short.MAX_VALUE)
 					.addComponent(lblSecretaria, GroupLayout.PREFERRED_SIZE, 151, GroupLayout.PREFERRED_SIZE)
 					.addGap(180))
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addContainerGap(222, Short.MAX_VALUE)
+					.addComponent(btnVolverAlMen, GroupLayout.PREFERRED_SIZE, 125, GroupLayout.PREFERRED_SIZE)
+					.addGap(217))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(171)
+					.addComponent(labelResultado, GroupLayout.PREFERRED_SIZE, 252, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(141, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -117,22 +165,28 @@ public class MorososMensualidad extends JFrame {
 						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
 					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 179, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(labelResultado, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addComponent(btnVolverAlMen)
-					.addGap(25))
+					.addContainerGap(18, Short.MAX_VALUE))
 		);
-		
 		table = new JTable();
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"Rut estudiante", "Cant. Cursos", "Mes", "Monto"
-			}
-		));
+		visible();
+		
 		scrollPane.setViewportView(table);
 		contentPane.setLayout(gl_contentPane);
 		
 	
+	}
+	
+	public  void visible(){
+		table.setModel(new DefaultTableModel(
+				new Object[][] {
+				},
+				new String[] {
+					"Rut estudiante", "Cant. Cursos", "Mes", "Monto"
+				}
+			));
 	}
 }
--- src/Vista/ReporteSueldoProfes.java
+++ src/Vista/ReporteSueldoProfes.java
@@ -8,21 +8,30 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
+
 import java.awt.Font;
+
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
+import com.google.gson.Gson;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.rmi.RemoteException;
+
+import serviciosueldo.ServicioSueldoProxy;;
 
 public class ReporteSueldoProfes extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField textRutProf;
 	private JTable table;
 
 	/**
@@ -46,7 +55,7 @@ public class ReporteSueldoProfes extends JFrame {
 	 */
 	public ReporteSueldoProfes() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 628, 570);
+		setBounds(100, 100, 628, 473);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -61,8 +70,8 @@ public class ReporteSueldoProfes extends JFrame {
 		
 		JLabel lblRutProfesor = new JLabel("Rut profesor:");
 		
-		textField = new JTextField();
-		textField.setColumns(10);
+		textRutProf = new JTextField();
+		textRutProf.setColumns(10);
 		
 		JButton btnVolverAlMen = new JButton("Volver al Men\u00FA");
 		btnVolverAlMen.addActionListener(new ActionListener() {
@@ -75,9 +84,46 @@ public class ReporteSueldoProfes extends JFrame {
 		});
 		
 		JButton btnVer = new JButton("Ver");
+		btnVer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				visible();
+				
+				String rutProf = textRutProf.getText();
+				ServicioSueldoProxy sueldos = new ServicioSueldoProxy();
+				
+				String[][] matriz;
+				Gson gson = new Gson();
+				String jsonMorosos;
+				try {
+					jsonMorosos = sueldos.consSueldoProf(rutProf);
+					if(jsonMorosos!=null){
+						matriz = gson.fromJson(jsonMorosos, String[][].class);
+						Object [] fila = new Object[5]; fila[0] = "unal";
+						
+						for (int i = 0; i < matriz.length; i++) {
+						fila[0] = matriz[i][0];
+						fila[1] = matriz[i][1];
+						fila[2] = matriz[i][2];
+						fila[3] = matriz[i][3];
+						fila[4] = matriz[i][4];
+
+					
+						((DefaultTableModel) table.getModel()).addRow(fila);
+					}
+						
+					}else {
+						JOptionPane.showMessageDialog(null, "Profesor no se encuentra o no registra sueldos");
+				}
+				} catch (RemoteException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+		});
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(240)
 					.addComponent(lblDirector, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE)
@@ -86,22 +132,22 @@ public class ReporteSueldoProfes extends JFrame {
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGap(195)
-							.addComponent(lblReporteSueldosProfesor, GroupLayout.DEFAULT_SIZE, 246, Short.MAX_VALUE))
+							.addComponent(lblReporteSueldosProfesor, GroupLayout.DEFAULT_SIZE, 344, Short.MAX_VALUE))
 						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGap(89)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 								.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 450, GroupLayout.PREFERRED_SIZE)
 								.addGroup(gl_contentPane.createSequentialGroup()
 									.addComponent(lblRutProfesor, GroupLayout.PREFERRED_SIZE, 83, GroupLayout.PREFERRED_SIZE)
 									.addPreferredGap(ComponentPlacement.UNRELATED)
-									.addComponent(textField)
+									.addComponent(textRutProf)
 									.addGap(18)
 									.addComponent(btnVer, GroupLayout.PREFERRED_SIZE, 69, GroupLayout.PREFERRED_SIZE)))))
 					.addGap(63))
-				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
-					.addGap(247)
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addContainerGap(256, Short.MAX_VALUE)
 					.addComponent(btnVolverAlMen)
-					.addContainerGap(252, Short.MAX_VALUE))
+					.addGap(243))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
@@ -113,25 +159,29 @@ public class ReporteSueldoProfes extends JFrame {
 					.addGap(21)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblRutProfesor)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textRutProf, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(btnVer))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 312, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 205, GroupLayout.PREFERRED_SIZE)
+					.addGap(36)
 					.addComponent(btnVolverAlMen)
-					.addContainerGap())
+					.addContainerGap(109, Short.MAX_VALUE))
 		);
 		
 		table = new JTable();
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"Apellido", "Mes", "Cant. cursos", "Monto", "Estado pago"
-			}
-		));
+		visible();
+		
 		scrollPane.setViewportView(table);
 		contentPane.setLayout(gl_contentPane);
 	}
 
+	public void visible(){
+		table.setModel(new DefaultTableModel(
+				new Object[][] {
+				},
+				new String[] {
+					"Apellido", "Mes", "Cant. cursos", "Monto", "Estado pago"
+				}
+			));
+	}
 }
